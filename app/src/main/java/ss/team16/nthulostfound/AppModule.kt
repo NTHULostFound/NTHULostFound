fed643751e23f92ab7c7c097ee918467b1db2133
Yi Kuo <kuokuoyiyi@gmail.com>
2022-05-30 17:30:56 +0800

    feat(DI): add Application & AppModule

---
+++ app/src/main/java/ss/team16/nthulostfound/AppModule.kt
@@ -0,0 +1,19 @@
+package ss.team16.nthulostfound
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideItemsRepository() {
+        // TODO
+    }
+
+}
---
+++ app/src/main/java/ss/team16/nthulostfound/NTHULostFound.kt
@@ -0,0 +1,9 @@
+package ss.team16.nthulostfound
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NTHULostFound : Application() {
+
+}
