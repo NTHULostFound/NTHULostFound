56d01b9a2d26309b5c7b41e071c48146f7fa41f1
Tony Yang <t510599@gmail.com>
2022-06-02 17:26:24 +0800

    update(UserData): add accessToken field

--- app/src/main/java/ss/team16/nthulostfound/domain/model/UserData.kt
+++ app/src/main/java/ss/team16/nthulostfound/domain/model/UserData.kt
@@ -3,9 +3,10 @@ package ss.team16.nthulostfound.domain.model
 import android.graphics.Bitmap
 
 data class UserData(
-    val avatar: Bitmap? = null,
     val uuid: String = "",
     val fcmToken: String = "",
+    val accessToken: String = "",
+    val avatar: Bitmap? = null,
     val name: String = "",
     val studentId: String = "",
     val email: String = "",
