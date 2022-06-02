c760eab47f112790150ebf3a9277c7808ab66750
Tony Yang <t510599@gmail.com>
2022-06-02 12:42:25 +0800

    refactor(ItemModule): merging ItemRepositoryModule and NewItemModule into ItemModule

--- app/src/main/java/ss/team16/nthulostfound/di/ItemModule.kt
+++ app/src/main/java/ss/team16/nthulostfound/di/ItemModule.kt
@@ -4,18 +4,34 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import ss.team16.nthulostfound.data.repository.ItemRepositoryMockImpl
 import ss.team16.nthulostfound.domain.repository.ItemRepository
 import ss.team16.nthulostfound.domain.usecase.GetItemUseCase
+import ss.team16.nthulostfound.domain.usecase.NewItemUseCase
 import ss.team16.nthulostfound.domain.usecase.ShareItemUseCase
+import ss.team16.nthulostfound.domain.usecase.UploadImagesUseCase
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ItemModule {
+
+    @Provides
+    @Singleton
+    fun provideItemRepository(): ItemRepository {
+        return ItemRepositoryMockImpl()
+    }
+
     @Provides
     @Singleton
     fun provideGetItemUseCase(itemRepository: ItemRepository): GetItemUseCase {
         return GetItemUseCase(itemRepository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewItemUseCase(uploadImagesUseCase: UploadImagesUseCase) : NewItemUseCase {
+        return NewItemUseCase(uploadImagesUseCase)
     }
 
     @Provides
