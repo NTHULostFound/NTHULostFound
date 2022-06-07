ff7641e7e42125b43c6b56a13f149d2e8a8d6b6c
Jerry Wu <a91082900@gmail.com>
2022-06-07 23:19:17 +0800

    feat(homescreen): use MultiFloatingActionButton made by TheMelody

---
+++ app/src/main/java/ss/team16/nthulostfound/ui/components/MultiFloatingActionButton.kt
@@ -0,0 +1,143 @@
+package ss.team16.nthulostfound.ui.components
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ss.team16.nthulostfound.ui.theme.Shapes
+
+enum class MultiFabState {
+    Collapsed,
+    Expanded
+}
+
+class MultiFabItem(
+    val icon: ImageVector,
+    val label: String,
+    val srcIconColor: Color = Color.White,
+    val labelTextColor: Color = Color.White,
+    val labelBackgroundColor: Color = Color.Black.copy(alpha = 0.6F),
+    val fabBackgroundColor: Color = Color.Unspecified,
+    val onClick: () -> Unit
+)
+
+@Composable
+fun MultiFloatingActionButton(
+    modifier: Modifier = Modifier,
+    srcIcon: ImageVector,
+    srcIconColor: Color = Color.White,
+    fabBackgroundColor: Color = Color.Unspecified,
+    showLabels: Boolean = true,
+    items: List<MultiFabItem>
+) {
+    val currentState = remember { mutableStateOf(MultiFabState.Collapsed) }
+    val transition = updateTransition(targetState = currentState, label = "")
+    val rotateAnim: Float by transition.animateFloat(
+        transitionSpec = {
+            if (targetState.value == MultiFabState.Expanded) {
+                spring(stiffness = Spring.StiffnessLow)
+            } else {
+                spring(stiffness = Spring.StiffnessMedium)
+            }
+        }, label = ""
+    ) { state ->
+        if (state.value == MultiFabState.Collapsed) 0F else -45F
+    }
+    val alphaAnim: Float by transition.animateFloat(transitionSpec = {
+        tween(durationMillis = 200)
+    }, label = "") { state ->
+        if (state.value == MultiFabState.Expanded) 1F else 0F
+    }
+    val shrinkListAnim:MutableList<Float> = mutableListOf()
+    items.forEachIndexed { index, _ ->
+        val shrinkAnim by transition.animateFloat(targetValueByState = { state ->
+            when (state.value) {
+                MultiFabState.Collapsed -> 5F
+                MultiFabState.Expanded -> (index + 1) * 60F + if(index == 0) 5F else 0F
+            }
+        }, label = "", transitionSpec = {
+            if (targetState.value == MultiFabState.Expanded) {
+                spring(stiffness = Spring.StiffnessLow,dampingRatio = 0.58F)
+            } else {
+                spring(stiffness = Spring.StiffnessMedium)
+            }
+        })
+        shrinkListAnim.add(index, shrinkAnim)
+    }
+    Box(modifier = modifier, contentAlignment = Alignment.BottomEnd) {
+        items.forEachIndexed{index, item ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(
+                        bottom = shrinkListAnim[index].dp,
+                        top = 5.dp,
+                        end = 30.dp
+                    )
+                    .alpha(animateFloatAsState(alphaAnim).value)
+            ) {
+                if (showLabels) {
+                    Text(
+                        item.label,
+                        color = item.labelTextColor,
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .clip(Shapes.medium)
+                            .alpha(animateFloatAsState(alphaAnim).value)
+                            .background(color = item.labelBackgroundColor)
+                            .padding(start = 6.dp, end = 6.dp, top = 4.dp, bottom = 4.dp)
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
+                FloatingActionButton(
+                    backgroundColor = if (item.fabBackgroundColor == Color.Unspecified) MaterialTheme.colors.secondary else item.fabBackgroundColor,
+                    modifier = Modifier.size(46.dp),
+                    onClick = {
+                        currentState.value = MultiFabState.Collapsed
+                        item.onClick()
+                    },
+                    elevation = FloatingActionButtonDefaults.elevation(
+                        defaultElevation = 2.dp,
+                        pressedElevation = 4.dp
+                    )
+                ) {
+                    Icon(
+                        modifier = Modifier.size(16.dp),
+                        imageVector = item.icon,
+                        tint = item.srcIconColor,
+                        contentDescription = item.label
+                    )
+                }
+            }
+        }
+        FloatingActionButton(
+            modifier = Modifier.padding(0.dp, end = 25.dp),
+            backgroundColor = if(fabBackgroundColor == Color.Unspecified) MaterialTheme.colors.secondary else fabBackgroundColor,
+            onClick = {
+                currentState.value =
+                    if (currentState.value == MultiFabState.Collapsed) MultiFabState.Expanded else MultiFabState.Collapsed
+            }) {
+            Icon(
+                imageVector = srcIcon,
+                modifier = Modifier.rotate(rotateAnim),
+                tint = srcIconColor,
+                contentDescription = null
+            )
+        }
+    }
+}
--- app/src/main/java/ss/team16/nthulostfound/ui/home/HomeScreen.kt
+++ app/src/main/java/ss/team16/nthulostfound/ui/home/HomeScreen.kt
@@ -1,30 +1,22 @@
 package ss.team16.nthulostfound.ui.home
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Help
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import ss.team16.nthulostfound.domain.model.ItemData
-import ss.team16.nthulostfound.domain.model.UploadedImage
-import ss.team16.nthulostfound.ui.components.BottomNav
-import ss.team16.nthulostfound.ui.components.HomeAppBar
-import ss.team16.nthulostfound.ui.components.ItemCard
-import ss.team16.nthulostfound.ui.theme.NTHULostFoundTheme
-import java.util.*
+import ss.team16.nthulostfound.ui.components.*
 
 @Composable
 fun HomeScreen(
@@ -42,82 +34,23 @@ fun HomeScreen(
             })
         },
         floatingActionButton = {
-            Column(
-                horizontalAlignment = Alignment.End
-            ) {
-                when (viewModel.fabState) {
-                    FabState.WITH_TEXT -> {
-                        ExtendedFloatingActionButton(
-                            text = {
-                                Text("新增")
-                            },
-                            icon = {
-                                Icon(
-                                    imageVector =
-                                    if(viewModel.fabState != FabState.EXTENDED) {
-                                        Icons.Filled.Add
-                                    } else {
-                                        Icons.Filled.Close
-                                    },
-                                    contentDescription = "add"
-                                )
-                            },
-                            onClick = {
-                                viewModel.onFabClicked()
-                            }
-                        )
-                    }
-                    FabState.EXTENDED -> {
-                        ExtendedFloatingActionButton(
-                            onClick = { navController.navigate("new_item/lost") },
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Filled.Help,
-                                    contentDescription = "add_lost"
-                                )
-                            },
-                            text = {
-                                Text(
-                                    text = "我東西掉了",
-                                    modifier = modifier.width(96.dp)
-                                )
-                            }
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-                        ExtendedFloatingActionButton(
-                            onClick = { navController.navigate("new_item/found") },
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Filled.Search,
-                                    contentDescription = "add_found"
-                                )
-                            },
-                            text = {
-                                Text(
-                                    text = "我撿到東西了",
-                                    modifier = modifier.width(96.dp)
-                                )
-                            }
-                        )
-                        Spacer(modifier = Modifier.height(20.dp))
-                        FloatingActionButton(onClick = { viewModel.onFabClicked() }) {
-                            Icon(
-                                imageVector = Icons.Filled.Close,
-                                contentDescription = "close"
-                            )
-                        }
-                    }
-                    FabState.COLLAPSED -> {
-                        FloatingActionButton(onClick = { viewModel.onFabClicked() }) {
-                            Icon(
-                                imageVector = Icons.Filled.Add,
-                                contentDescription = "add"
-                            )
-                        }
-                    }
-                }
-
-            }
+            val expandFabItemList: MutableList<MultiFabItem> = mutableListOf(
+                MultiFabItem(
+                    icon = Icons.Filled.Search,
+                    label = "我撿到東西了",
+                    fabBackgroundColor = Color.Black,
+                    onClick = { navController.navigate("new_item/found") }
+                ),
+                MultiFabItem(
+                    icon = Icons.Filled.Help,
+                    label = "我東西掉了",
+                    fabBackgroundColor = Color.Black,
+                    onClick = { navController.navigate("new_item/lost") }
+                )
+            )
+            MultiFloatingActionButton(
+                srcIcon = Icons.Filled.Add,
+                items = expandFabItemList)
         },
         bottomBar = {
             val showTypeStr = if(viewModel.showType == ShowType.FOUND) "found" else "lost"
